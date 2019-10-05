9d84b6867715519259874b20c9aa43ca851f767c
LAPTOP-R0D5PEQT\VON <kietnguyen2804@gmail.com>
2019-10-05 10:39:35 -0500

    fx:id Controller class

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,26 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private Button calcButton;
+
+    @FXML
+    private TextField number1;
+
+    @FXML
+    private TextField number2;
+
+    @FXML
+    private Label resultLabel;
+
+    @FXML
+    void calcSum(ActionEvent event) {
+
+    }
 }
